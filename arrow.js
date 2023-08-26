e0fe1ff24799f20ee6761bfb9d5f36f4f2c9c99b
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-26 02:49:38 -0700

    arrow funciton

---
+++ arrow.js
@@ -0,0 +1,26 @@
+const add = (x, y) => {
+    const result = x + y;
+    return result;
+}
+console.log(add(5, 10));
+
+const adds = (x, y) => x + y;
+console.log(adds(2, 3));
+
+const sum = (a, b) => a + b;
+console.log(sum(10, 20));
+
+const sub = (num1, num2) => num1 - num2;
+console.log(sub(40, 30));
+
+const show = () => {
+    console.log('hello js');
+}
+show();
+
+const showNumber = x => console.log(x);
+showNumber(455);
+
+const Number = x => Math.pow(x, 3);
+console.log(Number(4));
+
